0196b1b035c00875ca6e182e7f9b2e7fc6d07ad6
jake5987 <icecreamparlor04@gmail.com>
2024-12-31 11:59:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🛑 Watching Scammers ", "📛 Anti Scam"];
+const statusMessages = ["🍦 The Ice Cream Parlor ", "🎮 Playing The Ice Cream Parlor"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
